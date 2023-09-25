24106c6a1791fe10723324a1ae4726b117dce57c
NAPSTERSIHOTANG <cdsihotang12@gmail.com>
2023-09-25 14:35:28 +0700

    login

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,50 @@ import 'package:ugd1/View/login.dart';
 //import 'package:ugd1/View/register.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyHomePage());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+bool _iconbool = false;
+
+IconData _iconLight = Icons.wb_sunny;
+IconData _iconDark = Icons.nights_stay;
+
+ThemeData lightTheme = ThemeData(
+  primarySwatch: Colors.blue,
+  brightness: Brightness.light,
+);
+
+ThemeData darkTheme = ThemeData(
+  primarySwatch: Colors.indigo,
+  brightness: Brightness.dark,
+);
+
+class _MyHomePageState extends State<MyHomePage> {
+  ThemeData _currentTheme = lightTheme;
+
+  void _toggleTheme() {
+    setState(() {
+      _currentTheme = _currentTheme == lightTheme ? darkTheme : lightTheme;
+      _iconbool = !_iconbool;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: LoginView(),
-    );
-  }
+    return MaterialApp(
+      theme: _currentTheme,
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: _toggleTheme,
+          child: const Icon(Icons.wb_sunny),
+        ),
+        body: LoginView(),
+     ),
+);
+}
 }
